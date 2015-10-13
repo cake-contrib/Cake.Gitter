6416a99fdde8003071fb7203b0fa9bfb0dbe93e3
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-13 20:18:57 +0100

    Adding StyleCop to Project

--- Source/Cake.Gitter/GitterProvider.cs
+++ Source/Cake.Gitter/GitterProvider.cs
@@ -10,17 +10,23 @@ namespace Cake.Gitter
         private readonly object _chat;
 
         /// <summary>
-        /// The Slack Chat functionality.
-        /// </summary>
-        public object Chat { get { return _chat; } }
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="GitterProvider"/> class.
         /// </summary>
         /// <param name="context">The context.</param>
         public GitterProvider(ICakeContext context)
         {
             _chat = new object();
+        }
+
+        /// <summary>
+        /// The Slack Chat functionality.
+        /// </summary>
+        public object Chat
+        {
+            get
+            {
+                return _chat;
+            }
         }
     }
 }
