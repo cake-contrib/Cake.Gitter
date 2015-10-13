d9532d519c702fd9767332aa3f3affa9ab7975c0
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-13 13:02:49 +0100

    (build) Adding default project
    - Testing out default Cake Bootstrapping for restoring packages

---
+++ Source/Cake.Gitter/GitterAliases.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+
+namespace Cake.Slack
+{
+    /// <summary>
+    /// Contains aliases related to GitterProvider API
+    /// </summary>
+    [CakeAliasCategoryAttribute("Slack")]
+    public static class GitterAliases
+    {
+        /// <summary>
+        /// Gets a <see cref="GitterProvider"/> instance that can be used for communicating with GitterProvider API.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>A <see cref="GitterProvider"/> instance.</returns>
+        [CakePropertyAlias(Cache = true)]
+        [CakeNamespaceImportAttribute("Cake.Gitter.Chat")]
+        public static GitterProvider Gitter(this ICakeContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            return new GitterProvider(context);
+        }
+    }
+}
---
+++ Source/Cake.Gitter/GitterProvider.cs
@@ -0,0 +1,26 @@
+﻿using Cake.Core;
+
+namespace Cake.Slack
+{
+    /// <summary>
+    /// Contains functionality related to Gitter API
+    /// </summary>
+    public sealed class GitterProvider
+    {
+        private readonly object _chat;
+
+        /// <summary>
+        /// The Slack Chat functionality.
+        /// </summary>
+        public object Chat { get { return _chat; } }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GitterProvider"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public GitterProvider(ICakeContext context)
+        {
+            _chat = new object();
+        }
+    }
+}
