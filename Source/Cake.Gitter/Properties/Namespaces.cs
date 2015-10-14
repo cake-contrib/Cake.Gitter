a98fc8522e547caa4c19fa1afdbbc0eee10373b2
Gary Ewan Park <gep13@gep13.co.uk>
2015-10-14 22:13:38 +0100

    Adding namespaces for documentation creation
    - As suggested here by @devlead
    https://gitter.im/gep13/Cake.Gitter?at=561e57fe99bbd76f0f315361

---
+++ Source/Cake.Gitter/Properties/Namespaces.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+// ReSharper disable once CheckNamespace
+namespace Cake.Gitter
+{
+    /// <summary>
+    /// This namespace contains Gitter aliases.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
+
+// ReSharper disable once CheckNamespace
+namespace Cake.Gitter.Chat
+{
+    /// <summary>
+    /// This namespace contain types for the Gitter Chat tool.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
